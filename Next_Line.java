64a74915bfed48d8b71b166f2fb7207e3ccb5fcb
officialNamansharma <hey.namansharma@gmail.com>
2026-01-14 18:06:42 +0530

    Practice code for Next Line

---
+++ Next_Line.java
@@ -0,0 +1,8 @@
+public class nextline {
+ public static void main(String[] args){
+    System.out.println("hellow world");
+    System.out.println("hello nextline");
+    System.out.print("last line\n");
+    System.out.println("another next line");
+ }
+}
