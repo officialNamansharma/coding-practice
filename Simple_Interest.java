f950b7af2171edd5be2b86c77871814c0e566e8b
officialNamansharma <hey.namansharma@gmail.com>
2026-01-16 00:06:51 +0530

    Practice code for Simple Interest

---
+++ Simple_Interest.java
@@ -0,0 +1,9 @@
+public class simpleinterest {
+    public static void main(String[] args) {
+        double p = 1000;
+        double r = 10;
+        double t = 2;
+        double si = p*r*t/100;
+        System.out.println(+si);
+    }
+}
