169a3d49d373aab388bfecf232c3c96268a9eb42
officialNamansharma <hey.namansharma@gmail.com>
2026-01-16 19:02:20 +0530

    Practice code for Naming Variable

---
+++ Naming_Variable.java
@@ -0,0 +1,7 @@
+public class variablename {
+    public static void main(String[] args){
+        int $y = 6;
+        // special character not allowed in initilization and declaration accept $ and _
+        System.out.println($y);
+    }
+}
