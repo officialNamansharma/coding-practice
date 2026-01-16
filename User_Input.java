0d92f713ba90f1859f01ed65a62029b628f8f3b2
officialNamansharma <hey.namansharma@gmail.com>
2026-01-16 19:44:18 +0530

    Practice code for User Input

--- User_Input.java
+++ User_Input.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
-public class input {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(n*n);
+public class classname{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println(n);
     }
+
 }
