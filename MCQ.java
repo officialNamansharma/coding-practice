85b1790306a6730c5f7db339e8bc0e88d5fbb837
officialNamansharma <hey.namansharma@gmail.com>
2026-01-17 19:31:21 +0530

    Practice code for MCQ

---
+++ MCQ.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class classnmae
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int  x = sc.nextInt();
+        System.out.println(x);
+    }
+}
