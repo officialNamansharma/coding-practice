9ea2e39367804f4c38ae2d27bd5cd9b74526ec9b
officialNamansharma <hey.namansharma@gmail.com>
2026-01-17 19:52:10 +0530

    Practice code for Absolute Number

--- Absolute_Number.java
+++ Absolute_Number.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
 public class classname
-{
-    public static void main(String[] args)
+{ 
+    public static void main(Sring[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+      if (x<0) 
+      {
+       System.out.println(-(x)); 
+      } else {
         System.out.println(x);
-    }
-}
+      }
+    } 
+} 
