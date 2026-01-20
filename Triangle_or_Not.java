0cc85c85a7e1320d02fb9f509601edd670a392f5
officialNamansharma <hey.namansharma@gmail.com>
2026-01-21 01:33:57 +0530

    Practice code for Triangle or Not

--- Triangle_or_Not.java
+++ Triangle_or_Not.java
@@ -1,26 +1,14 @@
 import java.util.Scanner;
-public class classname{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        
-    if (a+b>c) 
-    {
+public class main{
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+      if ((a+b)>c && (b+c)>a && (c+a)>b) {
         System.out.println("Triangle");
-    } 
-    else if(b+c>a) 
-    {
-        System.out.println("Triangle");
-    } 
-    else if(c+a>b)
-    {
-        System.out.println("Triangle");
-    }
-    else 
-    {
+      } else {
         System.out.println("Not a Triangle");
-    }
+      }
     }
 }
