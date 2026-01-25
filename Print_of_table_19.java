048d24ec21593a98cb72a5c237eb9d3ef0ba07b7
officialNamansharma <hey.namansharma@gmail.com>
2026-01-26 00:48:01 +0530

    Practice code for Print of table 19

--- Print_of_table_19.java
+++ Print_of_table_19.java
@@ -3,9 +3,12 @@ public class main
 {
     public static void main(String[] args) 
     {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+
      for(int i=1 ; i<=10 ; i++)
      {
-        System.out.println(i*19);
+        System.out.println(i*n);
      }   
     }
 }
