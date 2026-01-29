0b9bd2ad0b0e630b034f409f23b8799748e35f84
Naman Sharma <hey.namansharma@gmail.com>
2026-01-30 03:54:56 +0530

    Practice code for Continue

---
+++ Continue.java
@@ -0,0 +1,21 @@
+//Wap to print odd number from 1 to 100
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        
+        
+        for(int i=1 ; i<=100 ; i++)
+        {
+            if (i%2 == 0) 
+            {
+               
+            } 
+            else 
+            {
+            System.out.println(i);    
+            }
+        }
+    }
+}
