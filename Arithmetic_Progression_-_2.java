9adefe06c216f478d0e86851c5fd0bc875d813de
Naman Sharma <hey.namansharma@gmail.com>
2026-01-29 23:54:12 +0530

    Practice code for Arithmetic Progression - 2

--- Arithmetic_Progression_-_2.java
+++ Arithmetic_Progression_-_2.java
@@ -3,6 +3,12 @@ public class main
 {
     public static void main(String[] args) 
     {
-        System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+     for(int i = 1 ; i <= n ; i++)
+     {
+      System.out.println(i);
+     }
+         
     }
 }
