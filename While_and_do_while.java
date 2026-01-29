42f68e236946878fbf38f6878982af6b813d1bb8
Naman Sharma <hey.namansharma@gmail.com>
2026-01-30 04:56:01 +0530

    Practice code for While and do while

---
+++ While_and_do_while.java
@@ -0,0 +1,15 @@
+//While loop
+import java.util.Scanner;
+public class main
+{
+    public static void main(String[] args) 
+    {
+
+    int i=1;
+    while(i<=10){
+        System.out.println(i);
+        i++;
+    }
+
+    }
+}
