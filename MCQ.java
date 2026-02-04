8d62353e5e94d2a3ff90c0a33c7cdf3d516bdbfb
Naman Sharma <hey.namansharma@gmail.com>
2026-02-04 23:48:44 +0530

    Practice code for MCQ

--- MCQ.java
+++ MCQ.java
@@ -1,11 +1,55 @@
 import java.util.Scanner;
-public class classnmae
-{
-    public static void main(String[] args)
-    {
+
+public class Calculator {
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        int  x = sc.nextInt();
-        System.out.println(x);
+        // Input first number
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        // Input second number
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        // Input operator
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        double result;
+
+        // Switch case for operations
+        switch (operator) {
+
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result = " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed!");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+        }
+
+        sc.close();
     }
 }
