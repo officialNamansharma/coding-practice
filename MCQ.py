b707c8b0234a556f03db85454ed98e8bd467c830
Naman Sharma <hey.namansharma@gmail.com>
2026-02-06 23:37:54 +0530

    Practice code for MCQ

---
+++ MCQ.py
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.println("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Choose an operation: +  -  *  /");
+        char op = sc.next().charAt(0);
+
+        double result;
+
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result = " + result);
+                } else {
+                    System.out.println("Cannot divide by zero!");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operation!");
+        }
+
+        sc.close();
+    }
+}
