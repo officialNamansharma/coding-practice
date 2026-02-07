057f921774737bb7509546e6eb176c4ea67a58fc
Naman Sharma <hey.namansharma@gmail.com>
2026-02-07 23:49:47 +0530

    Practice code for MCQ

--- MCQ.java
+++ MCQ.java
@@ -1,49 +1,19 @@
 import java.util.Scanner;
 
-public class Calculator {
+public class AddTwentyReflection {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter first number: ");
-        double num1 = sc.nextDouble();
+        // Taking input from user
+        System.out.print("Enter any number: ");
+        int num = sc.nextInt();
 
-        System.out.println("Enter second number: ");
-        double num2 = sc.nextDouble();
+        // Adding 20
+        int result = num + 20;
 
-        System.out.println("Choose an operation: +  -  *  /");
-        char op = sc.next().charAt(0);
-
-        double result;
-
-        switch (op) {
-            case '+':
-                result = num1 + num2;
-                System.out.println("Result = " + result);
-                break;
-
-            case '-':
-                result = num1 - num2;
-                System.out.println("Result = " + result);
-                break;
-
-            case '*':
-                result = num1 * num2;
-                System.out.println("Result = " + result);
-                break;
-
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                    System.out.println("Result = " + result);
-                } else {
-                    System.out.println("Cannot divide by zero!");
-                }
-                break;
-
-            default:
-                System.out.println("Invalid operation!");
-        }
+        // Reflecting output
+        System.out.println("After adding 20, the result is: " + result);
 
         sc.close();
     }
