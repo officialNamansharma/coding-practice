7f5ae6efdf13a44e7237b7cd81106a6cccdfe84a
Naman Sharma <hey.namansharma@gmail.com>
2026-02-11 23:33:15 +0530

    Practice code for Intro to loops

--- Intro_to_loops.java
+++ Intro_to_loops.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
-public class main
-{
-    public static void main(String[] args) 
-    {
-     Scanner sc = new Scanner(System.in);
-     int n = sc.nextInt();
-     for(int i=0 ; i<n ; i++ )
-     {
-        System.out.println("Hello world");
-     }
 
+public class AddTwentyReflection {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input from user
+        System.out.print("Enter any number: ");
+        int num = sc.nextInt();
+
+        // Adding 20
+        int result = num + 20;
+
+        // Reflecting output
+        System.out.println("After adding 20, the result is: " + result);
+
+        sc.close();
     }
 }
