20e045d650cd0fba933279b5dbf67e3a567a44e9
Naman Sharma <hey.namansharma@gmail.com>
2026-02-21 22:06:48 +0530

    Practice code for Intro To Java

--- Intro_To_Java.java
+++ Intro_To_Java.java
@@ -1,23 +1,6 @@
-import java.util.Scanner;
-
-public class SumProgram {
+public class main
+{
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        // Taking input from user
-        System.out.print("Enter first number: ");
-        int a = sc.nextInt();
-
-        System.out.print("Enter second number: ");
-        int b = sc.nextInt();
-
-        // Calculating sum
-        int sum = a + b;
-
-        // Printing result
-        System.out.println("Sum = " + sum);
-
-        sc.close();
+        System.out.println("Hello World");
     }
 }
