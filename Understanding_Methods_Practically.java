a7d452a1c6a8f3cafc70955aeab0f2e600c26fc1
Naman Sharma <hey.namansharma@gmail.com>
2026-02-23 23:54:58 +0530

    Practice code for Understanding Methods Practically

--- Understanding_Methods_Practically.java
+++ Understanding_Methods_Practically.java
@@ -1,31 +1,30 @@
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
-public class GuessNumber {
+public class NumberGuessingGame {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
 
-        int numberToGuess = random.nextInt(100) + 1; // 1 to 100
-        int userGuess = 0;
+        int numberToGuess = rand.nextInt(100) + 1; // 1 to 100
+        int guess;
         int attempts = 0;
 
-        System.out.println("Guess a number between 1 and 100");
+        System.out.println("Guess a number between 1 and 100:");
 
-        while (userGuess != numberToGuess) {
-            System.out.print("Enter your guess: ");
-            userGuess = scanner.nextInt();
+        do {
+            guess = sc.nextInt();
             attempts++;
 
-            if (userGuess < numberToGuess) {
-                System.out.println("Too low!");
-            } else if (userGuess > numberToGuess) {
-                System.out.println("Too high!");
+            if (guess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else if (guess < numberToGuess) {
+                System.out.println("Too low! Try again.");
             } else {
-                System.out.println("Correct! You guessed it in " + attempts + " attempts.");
+                System.out.println("Correct! You guessed in " + attempts + " attempts.");
             }
-        }
+        } while (guess != numberToGuess);
 
-        scanner.close();
+        sc.close();
     }
 }
