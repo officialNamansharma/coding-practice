5ee1cd50ea6186dbc80302ef32c68d605338200f
Naman Sharma <hey.namansharma@gmail.com>
2026-02-26 00:19:12 +0530

    Practice code for Understanding Methods Practically

--- Understanding_Methods_Practically.java
+++ Understanding_Methods_Practically.java
@@ -1,32 +1,14 @@
-import java.util.Random;
 import java.util.Scanner;
 
-public class NumberGuessingGame {
+public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
 
-        int numberToGuess = random.nextInt(100) + 1; // 1 to 100
-        int guess;
-        int attempts = 0;
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
 
-        System.out.println("Welcome to Number Guessing Game!");
-        System.out.println("Guess a number between 1 and 100");
+        System.out.println("Hello, " + name + "! Welcome to Java.");
 
-        do {
-            System.out.print("Enter your guess: ");
-            guess = scanner.nextInt();
-            attempts++;
-
-            if (guess < numberToGuess) {
-                System.out.println("Too low!");
-            } else if (guess > numberToGuess) {
-                System.out.println("Too high!");
-            } else {
-                System.out.println("Correct! You guessed it in " + attempts + " attempts.");
-            }
-        } while (guess != numberToGuess);
-
-        scanner.close();
+        sc.close();
     }
 }
