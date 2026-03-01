880d28359073a468453e0459022569d9986c979c
Naman Sharma <hey.namansharma@gmail.com>
2026-03-01 22:02:27 +0530

    Practice code for Understanding Methods Practically

--- Understanding_Methods_Practically.java
+++ Understanding_Methods_Practically.java
@@ -1,39 +1,30 @@
+import java.util.Random;
 import java.util.Scanner;
 
-public class StudentGradeCalculator {
+public class GuessTheNumber {
     public static void main(String[] args) {
+        Random rand = new Random();
         Scanner sc = new Scanner(System.in);
 
-        int subjects;
-        System.out.print("Enter number of subjects: ");
-        subjects = sc.nextInt();
+        int numberToGuess = rand.nextInt(100) + 1; // 1 to 100
+        int userGuess = 0;
+        int attempts = 0;
 
-        int total = 0;
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("Guess a number between 1 and 100:");
 
-        for (int i = 1; i <= subjects; i++) {
-            System.out.print("Enter marks for subject " + i + ": ");
-            int marks = sc.nextInt();
-            total += marks;
+        while (userGuess != numberToGuess) {
+            userGuess = sc.nextInt();
+            attempts++;
+
+            if (userGuess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Correct! You guessed it in " + attempts + " attempts.");
+            }
         }
-
-        double average = (double) total / subjects;
-        char grade;
-
-        if (average >= 90) {
-            grade = 'A';
-        } else if (average >= 75) {
-            grade = 'B';
-        } else if (average >= 60) {
-            grade = 'C';
-        } else if (average >= 50) {
-            grade = 'D';
-        } else {
-            grade = 'F';
-        }
-
-        System.out.println("\nTotal Marks: " + total);
-        System.out.println("Average: " + average);
-        System.out.println("Grade: " + grade);
 
         sc.close();
     }
