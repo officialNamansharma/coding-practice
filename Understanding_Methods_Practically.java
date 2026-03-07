315c1543118735e7de84581eaabab7f94a102a96
Naman Sharma <hey.namansharma@gmail.com>
2026-03-07 20:32:38 +0530

    Practice code for Understanding Methods Practically

--- Understanding_Methods_Practically.java
+++ Understanding_Methods_Practically.java
@@ -1,9 +1,13 @@
-public class AddNumbers {
+public class NumberSum {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int sum = a + b;
 
-        System.out.println("Sum = " + sum);
+        int sum = 0;
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println("Number: " + i);
+            sum = sum + i;
+        }
+
+        System.out.println("Total Sum = " + sum);
     }
 }
