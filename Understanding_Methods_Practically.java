70610d73301c2e526604c136ed6c28108a1e86b0
Naman Sharma <hey.namansharma@gmail.com>
2026-03-09 23:59:12 +0530

    Practice code for Understanding Methods Practically

--- Understanding_Methods_Practically.java
+++ Understanding_Methods_Practically.java
@@ -1,13 +1,13 @@
-public class NumberSum {
+public class EvenOddNumbers {
     public static void main(String[] args) {
 
-        int sum = 0;
-
         for(int i = 1; i <= 10; i++) {
-            System.out.println("Number: " + i);
-            sum = sum + i;
+            if(i % 2 == 0) {
+                System.out.println(i + " is Even");
+            } else {
+                System.out.println(i + " is Odd");
+            }
         }
 
-        System.out.println("Total Sum = " + sum);
     }
 }
