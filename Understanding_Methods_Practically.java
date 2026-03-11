3dc5f724567b08e4b07b3c24e5d139353315b20d
Naman Sharma <hey.namansharma@gmail.com>
2026-03-11 23:32:26 +0530

    Practice code for Understanding Methods Practically

--- Understanding_Methods_Practically.java
+++ Understanding_Methods_Practically.java
@@ -1,12 +1,8 @@
-public class EvenOddNumbers {
+public class PrintNumbers {
     public static void main(String[] args) {
 
         for(int i = 1; i <= 10; i++) {
-            if(i % 2 == 0) {
-                System.out.println(i + " is Even");
-            } else {
-                System.out.println(i + " is Odd");
-            }
+            System.out.println(i);
         }
 
     }
