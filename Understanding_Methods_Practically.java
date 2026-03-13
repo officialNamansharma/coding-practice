27f1667b5ca3ef192451d7040eb6705a58d2c38e
Naman Sharma <hey.namansharma@gmail.com>
2026-03-13 23:52:19 +0530

    Practice code for Understanding Methods Practically

---
+++ Understanding_Methods_Practically.java
@@ -0,0 +1,8 @@
+public class NumberSquares {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            int square = i * i;
+            System.out.println("Number: " + i + " Square: " + square);
+        }
+    }
+}
