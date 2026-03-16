fc7e0f0966f7b9ca1acf371e6b24b0075f92edba
Naman Sharma <hey.namansharma@gmail.com>
2026-03-16 23:36:48 +0530

    Practice code for Understanding Methods Practically

--- Understanding_Methods_Practically.java
+++ Understanding_Methods_Practically.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
-public class EvenOddCheck {
+public class EvenOdd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
 
-        if(num % 2 == 0) {
+        if (num % 2 == 0) {
             System.out.println("The number is Even.");
         } else {
             System.out.println("The number is Odd.");
