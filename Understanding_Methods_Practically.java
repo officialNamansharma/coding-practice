15b5906d0e027660bda984bdd2eaf7513f69b21a
Naman Sharma <hey.namansharma@gmail.com>
2026-04-06 02:25:17 +0530

    Practice code for Understanding Methods Practically

--- Understanding_Methods_Practically.java
+++ Understanding_Methods_Practically.java
@@ -1,75 +1,23 @@
-import java.util.ArrayList;
+// Find the Largest of three number in Java 
 import java.util.Scanner;
-
-class Student {
-    int id;
-    String name;
-    int age;
-
-    Student(int id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-    }
-
-    void display() {
-        System.out.println("ID: " + id + ", Name: " + name + ", Age: " + age);
-    }
-}
-
-public class StudentManagement {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        ArrayList<Student> students = new ArrayList<>();
 
-        int choice;
-
-        do {
-            System.out.println("\n--- Student Management System ---");
-            System.out.println("1. Add Student");
-            System.out.println("2. Display Students");
-            System.out.println("3. Exit");
-            System.out.print("Enter your choice: ");
-            
-            choice = sc.nextInt();
-
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter ID: ");
-                    int id = sc.nextInt();
-                    sc.nextLine(); // consume newline
-
-                    System.out.print("Enter Name: ");
-                    String name = sc.nextLine();
-
-                    System.out.print("Enter Age: ");
-                    int age = sc.nextInt();
-
-                    students.add(new Student(id, name, age));
-                    System.out.println("Student added successfully!");
-                    break;
-
-                case 2:
-                    if (students.isEmpty()) {
-                        System.out.println("No students found.");
-                    } else {
-                        System.out.println("\nStudent List:");
-                        for (Student s : students) {
-                            s.display();
-                        }
-                    }
-                    break;
-
-                case 3:
-                    System.out.println("Exiting program...");
-                    break;
-
-                default:
-                    System.out.println("Invalid choice!");
-            }
-
-        } while (choice != 3);
-
-        sc.close();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a>b && a>c) 
+        {
+            System.out.println("A is Greater");
+        } 
+        else if(b>a && b>c)
+        {
+            System.out.println("B is Greater");
+        } 
+        else if(c>a && c>b)
+        {
+            System.out.println("C is Greater");
+        }
     }
 }
