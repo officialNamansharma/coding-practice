40dd5ab64579e4a44fbbcd3019c6c5397b37c9a2
Naman Sharma <hey.namansharma@gmail.com>
2026-04-09 04:16:34 +0530

    Practice code for Understanding Methods Practically

--- Understanding_Methods_Practically.java
+++ Understanding_Methods_Practically.java
@@ -1,1 +1,25 @@
-//
+// Check whether a number is prime or not 
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int count=0;
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for(int i=2 ; i<=number-1 ; i++)
+        {
+            if(number%i == 0)
+            {
+                count = count+1;
+            }
+        }
+        if (count == 0) 
+        {
+         System.out.printf("Prime");    
+        } else 
+        {
+         System.out.println("Not Prime");   
+        }
+    }
+}
