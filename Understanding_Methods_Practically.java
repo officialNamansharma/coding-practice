e7255ecf6a12b418b9b11a86176d1a5920086a9e
Naman Sharma <hey.namansharma@gmail.com>
2026-04-23 20:42:31 +0530

    Practice code for Understanding Methods Practically

--- Understanding_Methods_Practically.java
+++ Understanding_Methods_Practically.java
@@ -3,11 +3,15 @@ public class main
 {
     public static void main(String[] args) 
     {
-        
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = a+b;
-        System.out.println(sum);
+        int n = sc.nextInt();
+        for(int i = 1 ; i<=n ; i++)
+        {
+            for( int j = n ; j>=i ; j--)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        } 
     }
 }
