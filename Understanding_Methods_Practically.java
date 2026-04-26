fbd6249f0fee2210a2f06cd348cc71e0448ae022
Naman Sharma <hey.namansharma@gmail.com>
2026-04-26 22:01:52 +0530

    Practice code for Understanding Methods Practically

--- Understanding_Methods_Practically.java
+++ Understanding_Methods_Practically.java
@@ -1,23 +1,17 @@
 import java.util.Scanner;
 public class main
 {
-public static void main(String[] args) 
-{
-   for(int i = 1 ; i<=5 ; i++)
-   {
-    for(int j = 1 ; j<=i ; j++)
-    {
-        System.out.print("x");
+    public static void main(String[] args) 
+    { 
+        
+        
+        for(int i=1 ; i<=4 ; i++)
+        {
+            for(int j=1 ; j<=i ; j++)
+            {
+                System.out.print("x");
+            }
+            System.out.println();
+        }
     }
-    System.out.println(" ");
-   }
-   for(int m = 1 ; m<=4 ; m++)
-   {
-    for(int n =4 ; n>=m ; n--)
-    {
-        System.out.print("x");
-    }
-    System.out.println(" ");
-   }    
-}
 }
