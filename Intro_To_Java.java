6a6d313195b807e4271eba28e15ed94e95fd760e
Naman Sharma <hey.namansharma@gmail.com>
2026-05-17 15:52:44 +0530

    Practice code for Intro To Java

--- Intro_To_Java.java
+++ Intro_To_Java.java
@@ -1,6 +1,4 @@
-public class main
-{
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+#import java.util.Scanner 
+public static void main(String[] args) {
+    
 }
